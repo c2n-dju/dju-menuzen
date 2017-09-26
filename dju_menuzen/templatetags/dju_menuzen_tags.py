4deabeb34e4f2c1a3713f3f2eccfc9db2a1b6201
Fabrice Pardo <fabrice.pardo@c2n.upsaclay.fr>
2017-09-26 11:47:28 +0200

    Initial commit of an old non-reviewed code

---
+++ dju_menuzen/templatetags/dju_menuzen_tags.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import with_statement
+
+import datetime
+from classytags.arguments import IntegerArgument, Argument, StringArgument
+from classytags.core import Options
+from classytags.helpers import InclusionTag
+from cms.utils.page_resolver import get_page_queryset
+from django import template
+from django.conf import settings
+from django.core.urlresolvers import reverse, NoReverseMatch
+from django.template.base import Node
+from django.template.defaultfilters import date
+from django.utils import timezone
+from django.utils.six.moves.urllib.parse import unquote
+from django.utils.translation import get_language
+from menus.menu_pool import menu_pool
+from menus.utils import DefaultLanguageChanger
+
+
+register = template.Library()
+
+
+class ShowMenuZen(InclusionTag):
+    """
+    Shows the menu_zen from the node that has the same url as the current request
+
+    - start_level: after which level should the breadcrumb start? 0=home
+    - template: template used to render the breadcrumb
+    """
+    name = 'show_menuzen'
+    template = 'menu/dummy.html'
+
+    options = Options(
+        Argument('start_level', default=0, required=False),
+        Argument('template', default='menu/breadcrumb.html', required=False),
+        Argument('only_visible', default=True, required=False),
+    )
+
+    def get_context(self, context, start_level, template, only_visible):
+        try:
+            # If there's an exception (500), default context_processors may not be called.
+            request = context['request']
+        except KeyError:
+            return {'template': 'cms/content.html'}
+        if not (isinstance(start_level, int) or start_level.isdigit()):
+            only_visible = template
+            template = start_level
+            start_level = 0
+        try:
+            only_visible = bool(int(only_visible))
+        except:
+            only_visible = bool(only_visible)
+        branch = []
+        nodes = menu_pool.get_nodes(request, breadcrumb=False) # fait appel à deepcopy, False pour appeler AuthVisibility.modify
+        selected = None
+        home = None
+        homeurl = unquote(reverse("pages-root"))
+        debug_allnodes = []
+        for node in nodes:
+            #print()
+            #print(node.title)
+            #print(node.visible)
+            #print(node.attr)
+            # debug_allnodes.append((node.title, node.selected))
+            if node.selected:
+                selected = node
+            if node.get_absolute_url() == homeurl:
+                home = node
+        if selected: #and selected != home:
+            node = selected
+            childnode = None
+            level = 0
+            color = 100
+            while node:
+                node.attr['color'] = "bread-" + str(color)
+                if node == home:
+                    node.attr['color'] = "bread-100"
+                if level < 3:
+                    color -= 20
+                    node.attr['display'] = node.title
+                else:
+                    node.attr['display'] = '…'
+                childs = []
+                for c in node.children:
+                    if c != childnode:
+                        c.attr['display'] = node.title
+                        c.attr['color'] = ''
+                    childs.append(c)
+                node.attr['childs'] = childs
+                if node.visible or not only_visible:
+                    branch.append(node)
+                level += 1
+                childnode = node
+                node = node.parent
+        if not branch or (branch and branch[-1] != home) and home:
+            branch.append(home)
+        branch.reverse()
+        if len(branch) >= start_level:
+            branch = branch[start_level:]
+        else:
+            branch = []
+        context.update({'branch': branch,
+                        'template': template,
+                        'homeurl': homeurl,
+                        'allnodes': debug_allnodes})
+        return context
+
+
+register.tag(ShowMenuZen)
